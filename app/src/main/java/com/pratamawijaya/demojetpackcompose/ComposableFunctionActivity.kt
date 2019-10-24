bb17eb5d4cf7bf1bba806fde0a015dfd3c86fb53
pratamawijaya <set.mnemonix@gmail.com>
2019-10-24 14:28:18 +0700

    tammbah rounded corner

--- app/src/main/java/com/pratamawijaya/demojetpackcompose/ComposableFunctionActivity.kt
+++ app/src/main/java/com/pratamawijaya/demojetpackcompose/ComposableFunctionActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.unaryPlus
+import androidx.ui.core.Clip
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.*
 import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
@@ -24,11 +26,13 @@ class ComposableFunctionActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Sapa(name:String) {
+fun Sapa(name: String) {
     val img = +imageResource(R.drawable.zombieland)
     Column(crossAxisSize = LayoutSize.Expand, modifier = Spacing(4.dp)) {
-        Container(expanded = true, height = 170.dp){
-            DrawImage(image = img)
+        Container(expanded = true, height = 170.dp) {
+            Clip(shape = RoundedCornerShape(8.dp)) {
+                DrawImage(image = img)
+            }
         }
         HeightSpacer(height = 16.dp)
         Text(text = "Hi $name")
@@ -40,6 +44,6 @@ fun Sapa(name:String) {
 
 @Preview
 @Composable
-fun PreviewSapa(){
+fun PreviewSapa() {
     Sapa(name = "Droid")
 }
