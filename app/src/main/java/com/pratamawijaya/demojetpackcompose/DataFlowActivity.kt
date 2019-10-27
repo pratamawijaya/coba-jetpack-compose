4cb0f8c5202b17a9e4b44ceb3e0263bccac84248
pratamawijaya <set.mnemonix@gmail.com>
2019-10-27 13:22:42 +0700

    add data flow screen

---
+++ app/src/main/java/com/pratamawijaya/demojetpackcompose/DataFlowActivity.kt
@@ -0,0 +1,51 @@
+package com.pratamawijaya.demojetpackcompose
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.compose.Composable
+import androidx.compose.Model
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.material.Button
+import androidx.ui.material.MaterialTheme
+
+class DataFlowActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MyApp {
+                DataFlowScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DataFlowScreen(appState: DataScreenState = DataScreenState()) {
+    Column {
+        Text(text = "Halaman State")
+        Counter(appState.counterState)
+    }
+}
+
+@Composable
+fun Counter(state: CounterState) {
+    Button(text = "Saya sudah klik ${state.count} kali", onClick = {
+        state.count++
+    })
+}
+
+@Composable
+fun MyApp(child: @Composable() () -> Unit) {
+    MaterialTheme {
+        child()
+    }
+}
+
+@Model
+class CounterState(var count: Int = 0)
+
+class DataScreenState(val counterState: CounterState = CounterState())
